83c2885d95e54647a8d41e898c79d95f4f45c6b3
Stuart Kennedy <rav2040@gmail.com>
2022-10-22 07:49:22 +1300

    Add more unit tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,28 @@
 import assert from "assert/strict";
+import { writeFileSync, unlinkSync } from "fs";
 import { resolve } from "node:path";
 import { createEnv } from "../src";
 
-const TEST_ENV = resolve(process.cwd(), "./tests/.test.env");
+const TEST_ENV = resolve(process.cwd(), "./tests/mocks/.test.env");
 
-// Test property is defined and equal to the expected value.
+// Does not throw when path is undefined.
+{
+  assert.doesNotThrow(createEnv);
+}
+
+// Test property is defined and equal to the expected value with default path.
+{
+  const filePath = resolve(process.cwd(), ".env");
+  writeFileSync(filePath, "TEST_PROPERTY=TEST_VALUE");
+
+  const Env = createEnv();
+  const testProperty = Env.get("TEST_PROPERTY");
+  assert.equal(testProperty, "TEST_VALUE");
+
+  unlinkSync(filePath);
+}
+
+// Test property is defined and equal to the expected value with custom path.
 {
   const Env = createEnv({ path: TEST_ENV });
   const testProperty = Env.get("TEST_PROPERTY");
@@ -30,4 +48,18 @@ const TEST_ENV = resolve(process.cwd(), "./tests/.test.env");
   const Env = createEnv({ path: TEST_ENV });
   const getTestProperty = () => Env.get("NON_EXISTANT_TEST_PROPERTY", true);
   assert.throws(getTestProperty);
+}
+
+// NODE_ENV is set to the expected value when not using override.
+{
+  const Env = createEnv({ path: TEST_ENV });
+  const testProperty = Env.get("NODE_ENV");
+  assert.equal(testProperty, "development");
+}
+
+// NODE_ENV is set to the expected value when using override.
+{
+  const Env = createEnv({ path: TEST_ENV, override: true });
+  const testProperty = Env.get("NODE_ENV");
+  assert.equal(testProperty, "test");
 }
